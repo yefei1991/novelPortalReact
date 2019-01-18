24230c213d402cf1b0671f60147becbe867ac191
yefei <2360274407@qq.com>
2019-01-18 17:57:59 +0800

    add first component

---
+++ src/yefei/Top.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Top.css';
+
+class Top extends Component {
+  render() {
+    return (
+      <div className="top">
+      </div>
+    );
+  }
+}
+
+class Button extends Component{
+  render(){
+    return(
+      
+    );
+  }
+}
+
+export default Top;
