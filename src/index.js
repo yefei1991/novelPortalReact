e151ba2762a25a3bb29381c6dd4307dbfafa02be
yefei <2360274407@qq.com>
2019-01-22 17:29:46 +0800

    secondCommit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Top from './yefei/Top';
+import Top from './yefei/components/Top';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Top />, document.getElementById('root'));
--- src/yefei/components/Top.js
+++ src/yefei/components/Top.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
+import Button from './Button'
 import './Top.css';
 
 class Top extends Component {
   render() {
     return (
       <div className="top">
+        <Button/>
       </div>
-    );
-  }
-}
-
-class Button extends Component{
-  render(){
-    return(
-      
     );
   }
 }
